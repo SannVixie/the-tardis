ac46012e8706e0fc1e6979b1a2f525d9615c495b
sue <sue@sue.com>
2017-02-17 23:23:14 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+		.wrapper {margin-top: 1.5em; text-align: center;}
+		.btn2{background: rgba(210,255,82,1); border-radius:0px !important; color:#333 !important; padding: 10px; text-decoration: none;}
+		.btn2:hover{background: rgba(145,232,66,1);border-radius:0px !important; color:#333 !important;}
+	</style>
+</head>
+<body>
+<br/><br/><br/>
+<div id="tardis">
+	<div id="lighttip"></div>
+	<div id="lighttop"></div>
+	<div id="light">
+		<div id ="lightmid"></div>
+	</div>
+	<div id="lightbase"></div>
+	<div id="topbase"></div>
+	<div id="toproof"></div>
+	<div id="topsign">
+		<div id="signpolice">
+			<p>POLICE</p>
+			<div id ="midsign">PUBLIC CALL</div>
+			<p>BOX</p>
+		</div>
+	</div>
+	<div id ="bluebox">
+		<div id ="bluedoorleft">
+			<a id="seehere"></a>
+			<div id="windowboxl1">
+				<div id="glass"></div>
+				<div id="glass"></div>
+				<div id="glass"></div>
+				<div id="glass"></div>
+				<div id="glass"></div>
+				<div id="glass"></div>
+			</div>
+			<div id="windowboxl2">
+				<div id="p1">
+					PUBLIC TELEPHONE<br>
+					FREE<br>
+					FOR USE OF<br>
+					PUBLIC<br>
+				</div>
+				<div id ="p2">
+					ADVICE & ASSISTANCE<br>
+					OBTAINABLE IMMEDIATELY<br><br>
+					OFFICER & CARS<br>
+					RESPOND TO ALL CALLS<br>
+				</div>
+				<div id="p2pto">
+					PULL TO OPEN
+				</div>
+			</div>
+			<div id="windowboxl1"></div>
+			<div id="windowboxl1"></div>
+		</div>
+		<div id ="bluedoorright">
+			<div id="windowboxright1">
+				<div id="glassr"></div>
+				<div id="glassr"></div>
+				<div id="glassr"></div>
+				<div id="glassr"></div>
+				<div id="glassr"></div>
+				<div id="glassr"></div>
+			</div>
+			<div id="windowboxright1"></div>
+			<div id="windowboxright1"></div>
+			<div id="windowboxright1"></div>
+		</div>
+	</div>
+	<div id="bottomstep"></div>
+
+</div>
+</body>
+</html>
